24dec1b08cd73f6a35b0cab72c81c5b54f8308e1
uyukihome <chenq11@rpi.edu>
2023-04-14 18:22:44 -0400

    1
    1

--- qianqi/history.py
+++ qianqi/history.py
@@ -5,7 +5,6 @@ from PyQt5.QtCore import pyqtSlot,QRect,QSize,pyqtSignal
 from PyQt5.QtWidgets import QTableWidget,QVBoxLayout,QHeaderView,QTableWidgetItem,QHeaderView,QSizePolicy,QAbstractScrollArea
 from database import Database
 import sqlite3
-<<<<<<< HEAD
 from fpdf import FPDF
 """
 History_UI class
@@ -17,9 +16,6 @@ class History_UI(QWidget):
     :param self:
     :return: None
     """
-=======
-class History_UI(QWidget):
->>>>>>> 92aaf0c9b4bb20663f1ad37244a9caa3d34b2ec5
     switch_to_the_main = pyqtSignal()
     def __init__(self):
         super().__init__()
@@ -30,7 +26,6 @@ class History_UI(QWidget):
         self.initUI()
 
   
-<<<<<<< HEAD
     """
     Set up the button for the history window, and set up the table for the window
     :param self:
@@ -38,16 +33,11 @@ class History_UI(QWidget):
     """
     def initUI(self):
         #set tilte for the page
-=======
-
-    def initUI(self):
->>>>>>> 92aaf0c9b4bb20663f1ad37244a9caa3d34b2ec5
         self.setWindowTitle(self.title)
         label = QLabel('History', self)
         label.resize(100, 30)
         label.move(290,0)
         label.setFont(QFont('Arial', 20))
-<<<<<<< HEAD
         #set button for back to menu
         button = QPushButton('Back to Menu', self)
         button.clicked.connect(self.switch_to_the_main.emit)
@@ -61,37 +51,22 @@ class History_UI(QWidget):
         button2.clicked.connect(self.aspdf)
         button2.setGeometry(110,0,110, 40)
         #create table to display database
-=======
-        button = QPushButton('Back to Menu', self)
-        button.clicked.connect(self.switch_to_the_main.emit)
-
-        #button.setToolTip('This is an example button')
-        button.setGeometry(535,0,110,40)
-        button1 = QPushButton('As TXT', self)
-        button1.clicked.connect(self.output)
-        button1.setGeometry(0,0,110,40)
->>>>>>> 92aaf0c9b4bb20663f1ad37244a9caa3d34b2ec5
         self.createTable()
         self.layout = QVBoxLayout()
         self.layout.insertSpacing(0,30)
         self.layout.addWidget(self.tableWidget) 
         self.setLayout(self.layout) 
         self.show()
-<<<<<<< HEAD
     """
     Center the window for the windown
     :param self:
     :return: None
     """
-=======
-
->>>>>>> 92aaf0c9b4bb20663f1ad37244a9caa3d34b2ec5
     def center(self):
         qr = self.frameGeometry()
         cp = QDesktopWidget().availableGeometry().center()
         qr.moveCenter(cp)
         self.move(qr.topLeft())
-<<<<<<< HEAD
     """
     The function which the download as txt button use to transfer the database file into a txt file
     :param self:
@@ -100,15 +75,10 @@ class History_UI(QWidget):
     def output(self):
         rows = self.data.selectAllRecords()
         #loop to put the data into txt version
-=======
-    def output(self):
-        rows = self.data.select_all_record()
->>>>>>> 92aaf0c9b4bb20663f1ad37244a9caa3d34b2ec5
         with open("output.txt", 'w') as f:
             f.write('{:<40}{:<40}{:<40}\n'.format("Time","Download", "Upload"))
             for i in range(len(rows)):
                 f.write('{:<40}{:<40}{:<40}\n'.format(rows[i][3],rows[i][1], rows[i][2]))
-<<<<<<< HEAD
     """
     The function which the download as pdf button use to transfer the database file into a pdf file
     :param self:
@@ -138,30 +108,32 @@ class History_UI(QWidget):
         self.tableWidget = QTableWidget()
         #set the initial table
         rows = self.data.selectAllRecords()
-=======
-    def createTable(self):
-        self.tableWidget = QTableWidget()
-        rows = self.data.select_all_record()
->>>>>>> 92aaf0c9b4bb20663f1ad37244a9caa3d34b2ec5
         self.tableWidget.setRowCount(len(rows))
         self.tableWidget.setColumnCount(3)
         self.tableWidget.setColumnWidth(0,190)
         self.tableWidget.setColumnWidth(1,190)
         self.tableWidget.setColumnWidth(2,190)
-<<<<<<< HEAD
         #set the column name for the table
-=======
->>>>>>> 92aaf0c9b4bb20663f1ad37244a9caa3d34b2ec5
         header = self.tableWidget.horizontalHeader()       
         header.setSectionResizeMode(0,QHeaderView.Stretch)
         header.setSectionResizeMode(1,QHeaderView.Stretch)
         header.setSectionResizeMode(2,QHeaderView.Stretch)
         self.tableWidget.setHorizontalHeaderLabels(('Time', 'Download', 'Upload'))
-<<<<<<< HEAD
         #for loop to input the dta into the database
-=======
->>>>>>> 92aaf0c9b4bb20663f1ad37244a9caa3d34b2ec5
         for x in range(len(rows)):
                 self.tableWidget.setItem(x, 0, QTableWidgetItem(str(rows[x][3])))
                 self.tableWidget.setItem(x, 1, QTableWidgetItem(str(rows[x][1])))
                 self.tableWidget.setItem(x, 2, QTableWidgetItem(str(rows[x][2])))
+    """
+    The function to update the table so that the table will change when user add more record into the database
+    :param self:
+    :return: None
+    """
+    def updateTable(self):
+        rows = self.data.selectAllRecords()
+        self.tableWidget.setRowCount(len(rows))
+        #for loop to update the data in the table
+        for x in range(len(rows)):
+            self.tableWidget.setItem(x, 0, QTableWidgetItem(str(rows[x][3])))
+            self.tableWidget.setItem(x, 1, QTableWidgetItem(str(rows[x][1])))
+            self.tableWidget.setItem(x, 2, QTableWidgetItem(str(rows[x][2])))
